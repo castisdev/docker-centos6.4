ebb29ac274e013974de96cff1c6f91153225c16b
alexyun <alexyun@castis.com>
2016-02-15 19:02:21 +0900

    update ca-certificate after adding devtoolset3 repository

--- install_devtoolset3.sh
+++ install_devtoolset3.sh
@@ -3,5 +3,6 @@ yum install -y scl-utils ; yum clean all -y
 wget https://www.softwarecollections.org/en/scls/rhscl/devtoolset-3/epel-6-x86_64/download/rhscl-devtoolset-3-epel-6-x86_64.noarch.rpm
 yum install -y rhscl-devtoolset-3-*.noarch.rpm ; yum clean all -y
 rm rhscl-devtoolset-3-*.noarch.rpm
+yum upgrade -y ca-certificates --disablerepo=epel ; yum clean all -y
 yum install -y devtoolset-3-gcc-c++ ; yum clean all -y
 yum install -y devtoolset-3-binutils ; yum clean all -y
