7271015a72bbabca13bb8c83ff52edeffebd6890
alexyun <alexyun@castis.com>
2016-06-22 16:46:30 +0900

    legacy_mode.sh devtoolset 경로 변경

--- legacy_mode.sh
+++ legacy_mode.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-export PATH=`echo $PATH | sed -e 's/\/opt\/rh\/devtoolset-3\/root\/usr\/bin\/://'`
+export PATH=`echo $PATH | sed -e 's/\/opt\/rh\/devtoolset-4\/root\/usr\/bin\/://'`
 unset BOOST_ROOT
 
