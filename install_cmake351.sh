ab8b301a74f96a9e48274596d056240022e1ba8d
sasgas <sasgas@castis.com>
2016-06-23 17:45:59 +0900

    try to fix "Rpmdb checksum is invalid: dCDPT(pkg checksums): ncurses-libs.x86_64 0:5.7-4.20090207.el6"

--- install_cmake351.sh
+++ install_cmake351.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 cd ~
+touch /var/lib/rpm/*
 yum -y install ncurses-devel; yum -y clean all
 wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.1.tar.gz
 tar xzf cmake-3.5.1.tar.gz
